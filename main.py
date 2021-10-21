831c41821b2f1c81722cdc715da1ccb8aff2720f
edupaiva <pro.eduardo@hotmail.com>
2021-10-21 12:47:50 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from flask import Flask, render_template, request, redirect, session, flash, url_for
+
+app = Flask(__name__)
+
+class cidade:
+    def __init__(self, nome, endereco):
+        self.nome = nome
+        self.endereco = endereco
+
+cidade1 = cidade('Fortaleza',"Rua X, n 40")
+cidade2 = cidade('Recife',"Rua X, n 40")
+cidade3 = cidade('Salvador',"Rua X, n 40")
+cidade4 = cidade('São Paulo',"Rua X, n 40")
+cidade5 = cidade('Rio de Janeiro',"Rua X, n 40")
+cidade6 = cidade('Porto Alegre',"Rua X, n 40")
+
+cidades = [cidade1,cidade2, cidade3,cidade4 , cidade5,cidade6 ]
+
+@app.get("/")
+async def root():
+    return render_template('index.html', titulo='Página Principal')
+
+@app.get("/estrutura")
+async def estr():
+    return render_template('estrutura.html', titulo='Estrutura')
+
+@app.get("/contato")
+async def cont():
+    return render_template('contato.html', titulo='Contato', cidades=cidades )
+
+
+app.run(debug=True)
---
+++ readme.txt
@@ -0,0 +1,6 @@
+#========================================================
+# Projeto de estudo usando Flask e renderização de páginas 
+# Html com Bootstrap e templates
+#
+#
+#========================================================
